97f32d6e8f87a0a0db5eeffee5c5f6c08e5fa8b5
ripeplantain <irvingmanny@gmail.com>
2023-11-16 17:52:59 +0000

    done with test for login and register route

---
+++ tests/Unit/LoginTest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class LoginTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    private $privateUrl = '/api/v1/login';
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        Artisan::call('passport:install');
+    }
+
+    /**
+     * Test a successful login.
+     *
+     * @return void
+     */
+    public function testSuccessfulLogin()
+    {
+        User::factory()->create([
+            'name' => 'John Doe',
+            'email' => 'unit@test.com',
+            'password' => bcrypt('password')
+        ]);
+
+        $data = [
+            'email' => 'unit@test.com',
+            'password' => 'password'
+        ];
+
+        $response = $this->postJson($this->privateUrl, $data);
+
+        $response->assertStatus(200)
+            ->assertJson([
+                'token' => true
+            ]);
+    }
+
+    /**
+     * Test a failed login due to invalid email.
+     *
+     * @return void
+     */
+    public function testFailedLoginInvalidEmail()
+    {
+        $response = $this->postJson($this->privateUrl, [
+            'email' => 'invalid@example.com',
+            'password' => 'password',
+        ]);
+
+        $response->assertStatus(404)
+            ->assertJson([
+                'message' => 'User not found',
+            ]);
+    }
+
+    /**
+     * Test a failed login due to invalid password.
+     *
+     * @return void
+     */
+    public function testFailedLoginInvalidPassword()
+    {
+        $user = User::factory()->create([
+            'password' => bcrypt('password'),
+        ]);
+
+        $response = $this->postJson($this->privateUrl, [
+            'email' => $user->email,
+            'password' => 'invalid',
+        ]);
+
+        $response->assertStatus(401)
+            ->assertJson([
+                'message' => 'Invalid Password',
+            ]);
+    }
+}
---
+++ tests/Unit/RegisterTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class RegisterTest extends TestCase
+{
+    use RefreshDatabase;
+
+    private $privateUrl = '/api/v1/register';
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        Artisan::call('passport:install');
+    }
+
+
+    public function testSuccessfulRegistration()
+    {
+        $data = [
+            'name' => 'John Doe',
+            'email' => 'johndoe@example.com',
+            'password' => 'password'
+        ];
+
+        $response = $this->postJson($this->privateUrl, $data);
+
+        $response->assertStatus(201)
+            ->assertJson([
+                'token' => true 
+            ]);
+
+        $this->assertDatabaseHas('users', [
+            'name' => 'John Doe',
+            'email' => 'johndoe@example.com'
+        ]);
+    }
+}
